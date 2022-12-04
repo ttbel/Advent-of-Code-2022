33a5444b4c2df0c9f87a3da334dfa9883bf1d6df
Tomasz Skowronek <tomek.s@larks.pl>
2022-12-04 19:37:50 +0100

    [day3] part2 solved

--- src/main/java/pl/larks/aoc22/day3/RucksackReorganization.java
+++ src/main/java/pl/larks/aoc22/day3/RucksackReorganization.java
@@ -35,4 +35,24 @@ public class RucksackReorganization {
 		return set.stream().map(l -> Character.isUpperCase(l) ? (int) l - (int) 'A' + 27 : (int) l - (int) 'a' + 1)
 				.reduce(Math::addExact);
 	}
+
+	Integer getPriorityOfTheItemsFromThreeRuckSacks(List<String> list) {
+
+		Integer summary = 0;
+		for (int i = 0; i < list.size(); i += 3) {
+			String l0 = list.get(i);
+			String l1 = list.get(i + 1);
+			String l2 = list.get(i + 2);
+			Set<Character> badge = new HashSet<>();
+
+			for (int j = 0; j < l0.length(); j++) {
+				if (l1.indexOf(l0.charAt(j)) != -1 && l2.indexOf(l0.charAt(j)) != -1) {
+					badge.add(l0.charAt(j));
+				}
+			}
+			final Optional<Integer> priotizeValue = computePriorityzes(badge).stream().reduce(Math::addExact);
+			summary += priotizeValue.get();
+		}
+		return summary;
+	}
 }
--- src/test/java/pl/larks/aoc22/day3/RucksackReorganizationTest.java
+++ src/test/java/pl/larks/aoc22/day3/RucksackReorganizationTest.java
@@ -13,7 +13,7 @@ class RucksackReorganizationTest {
 	FileUtils fu = new FileUtils();
 
 	@Test
-	void part1GetPriorityOfTheItems() {
+	void part1GetPriorityOfTheItemsForExample() {
 
 		RucksackReorganization r = new RucksackReorganization();
 
@@ -21,5 +21,38 @@ class RucksackReorganizationTest {
 		final int priorityOfTheItems = r.getPriorityOfTheItems(input);
 
 		assertThat(priorityOfTheItems).isEqualTo(157);
+	}
+
+	@Test
+	void part1GetPriorityOfTheItemsForInput() {
+
+		RucksackReorganization r = new RucksackReorganization();
+
+		List<String> input = fu.loadFileToList("inputs/day3/d3_1_input.txt");
+		final int priorityOfTheItems = r.getPriorityOfTheItems(input);
+
+		assertThat(priorityOfTheItems).isEqualTo(7863);
+	}
+
+	@Test
+	void part2GetPriorityOfTheItemsFromThreeRuckSacksForExample() {
+
+		RucksackReorganization r = new RucksackReorganization();
+
+		List<String> input = fu.loadFileToList("inputs/day3/d3_1_example.txt");
+		final int priorityOfTheItems = r.getPriorityOfTheItemsFromThreeRuckSacks(input);
+
+		assertThat(priorityOfTheItems).isEqualTo(70);
+	}
+
+	@Test
+	void part2GetPriorityOfTheItemsFromThreeRuckSacksForInput() {
+
+		RucksackReorganization r = new RucksackReorganization();
+
+		List<String> input = fu.loadFileToList("inputs/day3/d3_1_input.txt");
+		final int priorityOfTheItems = r.getPriorityOfTheItemsFromThreeRuckSacks(input);
+
+		assertThat(priorityOfTheItems).isEqualTo(2488);
 	}
 }
