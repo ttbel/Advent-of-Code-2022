6ed439e123a085e067e7db6c12c3568d9be46fe4
Tomasz Skowronek <tomek.s@larks.pl>
2022-12-06 00:59:01 +0100

    [day5] init

---
+++ src/main/java/pl/larks/aoc22/day5/SupplyStacks.java
@@ -0,0 +1,13 @@
+package pl.larks.aoc22.day5;
+
+import java.util.List;
+
+public class SupplyStacks {
+
+	String part1(List<String> list) {
+
+		final String result = "ABC";
+
+		return result;
+	}
+}
---
+++ src/test/java/pl/larks/aoc22/day5/SupplyStacksTest.java
@@ -0,0 +1,23 @@
+package pl.larks.aoc22.day5;
+
+import java.util.List;
+import java.util.Queue;
+
+import org.junit.jupiter.api.Test;
+
+import pl.larks.aoc22.utils.FileUtils;
+
+class SupplyStacksTest {
+
+	FileUtils fu = new FileUtils();
+
+	@Test
+	void part1WhenExample() {
+
+		final String fileName = "inputs/day5/d5_1_example.txt";
+		List<String> list = fu.loadFileToList(fileName);
+		fu.printInputStream(fileName);
+
+		Queue q
+	}
+}
